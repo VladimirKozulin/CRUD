34f44583c098786d90be28cced2e77194a85c663
Vladimir Kozulin <vladimir.kozulin.18@yandex.ru>
2024-08-26 16:00:06 +0300

    Настроил OpenApi и немного поигрался с описанием классов и методов

---
+++ src/main/java/com/example/rest/rest/configuration/OpenApiConfiguration.java
@@ -0,0 +1,43 @@
+package com.example.rest.rest.configuration;
+
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+//http://localhost:8080/swagger-ui/index.html
+@Configuration
+public class OpenApiConfiguration {
+
+    @Bean
+    public OpenAPI openApiDescription() {
+        Server localhostServer = new Server();
+        localhostServer.setUrl("http://localhost:8080");
+        localhostServer.setDescription("Localhost env");
+
+        Server productionServer = new Server();
+        productionServer.setUrl("http://some.prod.upl");
+        productionServer.setDescription("Production env");
+
+        Contact contact = new Contact();
+        contact.setName("Vladimir Kozulin");
+        contact.setEmail("vladimir.kozulin.18@yandex.ru");
+        contact.setUrl("https://github.com/VladimirKozulin");
+
+        License license = new License().name("GNU AGPLv3")
+                .url("https://choosealicense.com/licesense/agpl-3.0/");
+
+        Info info = new Info()
+                .title("Client orders API")
+                .version("1.0.0")
+                .contact(contact)
+                .description("API for client orders")
+                .termsOfService("http://some.terms.url")
+                .license(license);
+        return new OpenAPI().info(info).servers(List.of(localhostServer, productionServer));
+    }
+}
--- src/main/java/com/example/rest/rest/web/controller/v1/ClientController.java
+++ src/main/java/com/example/rest/rest/web/controller/v1/ClientController.java
@@ -1,12 +1,17 @@
 package com.example.rest.rest.web.controller.v1;
 
-import com.example.rest.rest.exception.EntityNotFoundException;
 import com.example.rest.rest.mapper.v1.ClientMapper;
 import com.example.rest.rest.model.Client;
 import com.example.rest.rest.service.ClientService;
 import com.example.rest.rest.web.model.ClientListResponse;
 import com.example.rest.rest.web.model.ClientResponse;
+import com.example.rest.rest.web.model.ErrorResponse;
 import com.example.rest.rest.web.model.UpsertClientRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,22 +21,38 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/client")
 @RequiredArgsConstructor
+@Tag(name = "Client V1", description = "Client API version V1") //Open API -> Описание класса
 public class ClientController {
     private final ClientService clientService;
 
     private final ClientMapper clientMapper;
 
+
+    @Operation(
+            summary = "Get clients",
+            description = "Get all clients",
+            tags = {"client"}
+    )
     @GetMapping
     public ResponseEntity<ClientListResponse> findAll() {
         return ResponseEntity.ok(
                 clientMapper.clientListToClientResponseList(clientService.findAll()));
     }
 
+
+
+    @Operation( //Описание операции
+            summary = "Get client by ID",
+            description = "Get client by ID. Return ID, name and list of orders",
+            tags = {"client","id"}
+    )
     @GetMapping("/{id}")
     public ResponseEntity<ClientResponse> findById(@PathVariable Long id) {
         return ResponseEntity.ok(
                 clientMapper.clientToResponse(clientService.findById(id)));
     }
+
+
     @PostMapping
     public ResponseEntity<ClientResponse> create(@RequestBody @Valid UpsertClientRequest request) {
         Client newClient = clientService.save(clientMapper.requestToClient(request));
@@ -39,6 +60,8 @@ public class ClientController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(clientMapper.clientToResponse(newClient));
     }
+
+
     @PutMapping("/{id}")
     public ResponseEntity<ClientResponse> update(@PathVariable("id") Long clientId,
                                                  @RequestBody UpsertClientRequest request) {
@@ -46,6 +69,13 @@ public class ClientController {
 
         return ResponseEntity.ok(clientMapper.clientToResponse(updateClient));
     }
+
+
+    @Operation(
+            summary = "Delete client by ID",
+            description = "Delete client by ID",
+            tags = {"client","id"}
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
         clientService.deleteById(id);
